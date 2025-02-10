23f9fb6a7ea8968c31e6eff106cdd6ee19db16c6
Ezz <ezzelsherbiny1999@gmail.com>
2025-02-10 17:17:28 +0200

    Update Cal.h

--- Project_4_Small_Cal/Cal.h
+++ Project_4_Small_Cal/Cal.h
@@ -9,5 +9,5 @@
 #ifndef CAL_H_
 #define CAL_H_
 void __Memset__ (unsigned char *ptr,unsigned char valu,unsigned char _size_);
-void Num_uCreate (unsigned char arr[],unsigned char counter,unsigned long *Num);
+long long Num_uCreate (unsigned char arr[],unsigned char counter);
 #endif /* CAL_H_ */
